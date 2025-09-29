1bf4f9c31788700e15967ba479872254c46ff6e4
ander.iglesias.ruiz <ander.iglesias.ruiz@opendeusto.es>
2025-09-29 09:38:09 +0200

    Añadido menu aplicación

--- EjemploSwingGit/src/VentanaSwingGit.java
+++ EjemploSwingGit/src/VentanaSwingGit.java
@@ -1,4 +1,11 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class VentanaSwingGit extends JFrame{
 
@@ -12,6 +19,26 @@ public class VentanaSwingGit extends JFrame{
         setSize(640, 480);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        
+        JMenuBar barraMenu = new JMenuBar();
+        setJMenuBar(barraMenu);
+        
+        JMenu menuFichero = new JMenu("Fichero");
+        barraMenu.add(menuFichero);
+        
+        JMenuItem itemMostrar = new JMenuItem("Mostrar");
+        menuFichero.add(itemMostrar);
+        JMenuItem itemLimpiar = new JMenuItem("Limpiar");
+        menuFichero.add(itemLimpiar);
+        JMenuItem itemSalir = new JMenuItem("Salir");
+        menuFichero.add(itemSalir);
+        
+        JTabbedPane panelPestanas = new JTabbedPane();
+        add(panelPestanas, BorderLayout.CENTER);
+        
+        panelPestanas.addTab("Datos Personales", new JPanel());
+        panelPestanas.add("Informacion", new JPanel());
+        panelPestanas.add("Comentarios", new JPanel());
 	}
 
 	public static void main(String[] args) {
